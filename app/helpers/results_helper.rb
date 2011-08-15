cc1ed4c071320deafda993c0781ea7f49cc3730f
John Perkins <john@silicontally.com>
2011-08-15 09:31:29 -0400

    Enhanced Result Set helper to display integer values

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,17 +1,17 @@
 module ResultsHelper
   def display_response(r_set,question)
-    sets = r_set.responses.select{|r| r.question.display_order == question.display_order}
-	  	if sets.size == 0
-  			return "-"
-  		elsif sets.size == 1
-  			return raw(sets.first.string_value || sets.first.text_value || show_answer(sets.first))
-  		else
-  		  txt = ""
-        sets.each do |set|
-          txt << show_answer(set) + "<br/>"
-        end
-        return raw(txt)
-		  end
+    sets = r_set.responses.select{|r| r.question.id == question.id} # Replaced display_order with id
+    if sets.size == 0
+      return "-"
+    elsif sets.size == 1
+      return raw(sets.first.string_value || sets.first.text_value || sets.first.integer_value || show_answer(sets.first))
+    else
+      txt = ""
+      sets.each do |set|
+        txt << show_answer(set) + "<br/>"
+      end
+      return raw(txt)
+    end
   end
   
   def show_answer(set)
